82b0dc3ece92e4787882288ca9caff34c0192e00
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2022-04-08 17:00:46 +1000

    random matrix sketch

--- sketch-01.js
+++ sketch-01.js
@@ -1,13 +1,39 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.lineWidth = width * 0.01;
+    const w = width * 0.1;
+    const h = height * 0.1;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+    const off = width * 0.01;
+    let x, y;
+    
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
+
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.stroke();
+        if ( 0.5 < Math.random()){
+          context.beginPath();
+          context.rect(x + off, y + off, w - (off*2), h - (off*2));
+          context.stroke();
+        }
+      }
+    }
+    /** **/
   };
 };
 
