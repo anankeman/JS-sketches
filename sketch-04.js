357ce0864453b0c17f1ec21964a312021826abdc
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2022-04-09 20:53:58 +1000

    Add linecap, animate, and frame rate control options

--- sketch-04.js
+++ sketch-04.js
@@ -12,7 +12,10 @@ const params = { cols: 10,
                 scaleMin: 1, 
                 scaleMax: 30, 
                 freq: 0.001, 
-                amp: 0.2};
+                amp: 0.2, 
+                frame: 0,
+                animate: true, 
+              lineCap: 'butt'};
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
@@ -39,7 +42,12 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x + frame * 10, y, params.freq);
+      // 2D noise
+      //const n = random.noise2D(x + frame * 10, y, params.freq);
+      // 3D noise
+      const f = params.animate ? frame : params.frame;
+      const n = random.noise3D(x, y, f * 10, params.freq);
+
       const angle = n * Math.PI * params.amp;
       //const scale = (n + 1) / 2 * 30;
       const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
@@ -50,6 +58,7 @@ const sketch = () => {
       context.rotate(angle);
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
@@ -67,6 +76,7 @@ const createPane = () => {
   const pane = new Tweakpane.Pane();
   let folder; 
   folder = pane.addFolder({title: 'Grid '});
+  folder.addInput(params, 'lineCap', {options: { butt: 'butt', round: 'round', square: 'square'}});
   folder.addInput(params, 'cols', {min: 2, max: 50, step: 1});
   folder.addInput(params, 'rows', {min: 2, max: 50, step: 1});
   folder.addInput(params, 'scaleMin', {min: 1, max: 100});
@@ -75,6 +85,8 @@ const createPane = () => {
   folder = pane.addFolder({title: "Noise ",});
   folder.addInput(params, 'freq', {min: -0.01, max: 0.01});
   folder.addInput(params, 'amp', {min: 0, max: 1});
+  folder.addInput(params, 'animate');
+  folder.addInput(params, 'frame', {min: 0, max: 999});
 
 }
 
