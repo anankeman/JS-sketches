86e1c9be9b7cfa1ed76d3a0defebaee61c8e33f3
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2022-04-13 17:24:11 +1000

    build text with text random positions

--- sketch-05.js
+++ sketch-05.js
@@ -1,4 +1,25 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+
+const loadMeSomeImage = () => {
+  //const url = 'https://img.icons8.com/glyph-neue/344/twitter.png';
+  const url = 'https://picsum.photos/200';
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.src = url;
+  });
+};
+
+const getImage = async () => {
+  const im = await loadMeSomeImage();
+  document.getElementById('body').appendChild(im);
+  //return im;
+}
+
+getImage();
+
 
 const settings = {
   dimensions: [ 1080,1080 ]
@@ -50,14 +71,18 @@ const sketch = ({ context, width, height }) => {
     typeContext.stroke();
 
     typeContext.fillText(text, 0, 0);
+    //typeContext.drawImage(im, 0, 0);
     typeContext.restore();
     const typeData = typeContext.getImageData(0,0, cols, rows).data;
     //console.log(typeData);
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
 
+    //context.drawImage(typeCanvas, 0, 0);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
 
-    context.drawImage(typeCanvas, 0, 0);
-
-    for (let i=0; i < numCells; i++) {
+    for (let i = 0; i < numCells; i++) {
       const col = i % cols;
       const row = Math.floor(i / cols);
       const x = col * cell;
@@ -68,23 +93,41 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2];
       const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      const glyph = getGlyph(r);
+      context.font = `${cell * 2}px ${fontFamily}`;
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+      //context.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
+      context.fillStyle = 'white';
 
       context.save();
       context.translate(x, y);
       context.translate(cell * 0.5, cell * 0.5);
       //context.fillRect(0, 0, cell, cell);
 
-      context.beginPath();
+      /* context.beginPath();
       context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
       context.fill();
-
+      */
+      context.fillText(glyph, 0, 0);
       context.restore();
 
     }
 
   };
 };
+
+
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '.';
+  if (v < 150) return '-';
+  if (v < 200) return '+';
+
+  const glyphs = '_= /'.split('');
+
+  return random.pick(glyphs);
+}
 
 const onKeyUp = (e) => {
   text = e.key.toUpperCase();
@@ -96,6 +139,8 @@ document.addEventListener('keyup', onKeyUp);
 const start = async () => {
   manager = await canvasSketch(sketch, settings);
 }
+
+
 
 start();
 
